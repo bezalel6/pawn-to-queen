6f5c00dfcf7e29a5a61d82b11546f95a29bbf642
bezalel6 <bezalel3250@gmail.com>
2025-11-13 09:15:28 +0200

    Remove unoptimized images configuration from Next.js config

--- next.config.js
+++ next.config.js
@@ -8,9 +8,6 @@ import "./src/env.js";
 const config = {
   reactStrictMode: true,
   output: "export",
-  images: {
-    unoptimized: true,
-  },
   basePath: process.env.NODE_ENV === "production" ? "/pawn-to-queen" : "",
   assetPrefix: process.env.NODE_ENV === "production" ? "/pawn-to-queen/" : "",
 
